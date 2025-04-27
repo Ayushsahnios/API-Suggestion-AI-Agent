8fcd07a4240432b98deee1761194a2753f5dd66c
Ayush Sahni <ayushsahni@Ayushs-MacBook-Pro.local>
2025-04-27 11:26:00 -0500

    2 Add gunicorn for production

--- server.py
+++ server.py
@@ -130,4 +130,5 @@ def save_code_to_files_and_zip(code_text):
     return zip_buffer
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
